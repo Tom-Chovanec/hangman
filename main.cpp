1fbe6402cbb35ccf75b28a9276324ddc7ecc176f
Tom-Chovanec <chovanectomas07@gmail.com>
2024-03-15 13:24:10 +0100

    added win text

--- main.cpp
+++ main.cpp
@@ -8,18 +8,19 @@ bool stringContainsChar(std::string str, char c);
 int main() {
     string word = "apple";
     string guess = "";
-    char a;
+    char tmp;
     while (guess != word)  {
         for (int i = 0; i < word.size(); i++) {
             if (!stringContainsChar(guess, word[i])) cout << "_";
             else cout << word[i];
         }
-        cin >> a;
-        guess += a;
+        cout << "     ";
+        cin >> tmp;
+        guess += tmp;
 
     }
+    cout << "YOU WON";
 }
-
 bool stringContainsChar(std::string str, char c) {
     return str.find(c) != std::string::npos;
 }
