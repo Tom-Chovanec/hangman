0557898c27413903906dd2ed94455977fe66edf6
Tom-Chovanec <118990261+Tom-Chovanec@users.noreply.github.com>
2024-03-15 13:08:54 +0100

    ?

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+bool stringContainsChar(std::string str, char c);
+
 int main() {
-    std::string word = "apple";
-    char guess = "";
+    string word = "apple";
+    string guess = '';
     while (guess != word)  {
+        for (int i = 0; i < word.size(); i++) {
+            if (!stringContainsChar(guess, word[i])) cout << "_";
+            else cout << word[i];
+        }
+        cin >> guess;
 
     }
+}
+
+bool stringContainsChar(std::string str, char c) {
+    return str.find(c) != std::string::npos;
 }
