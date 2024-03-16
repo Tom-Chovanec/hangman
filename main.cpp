798e6f4669d32b18deab64f6137ce2acd5594cc0
Tom-Chovanec <chovanectomas07@gmail.com>
2024-03-16 16:06:19 +0100

    added the hangman_txt to a render function

--- main.cpp
+++ main.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
+#include "render.cpp"
 
 using namespace std;
 
 bool stringContainsChar(std::string str, char c);
 
 int main() {
+    renderAscii(1);
+    cout << "press ANY BUTTON to start\n";
+    _getch();
     string word = "apple";
     string guess = "";
+    bool guessed = false;
     char tmp;
-    while (guess != word)  {
+    while (!guessed)  {
+        guessed = true;
         for (int i = 0; i < word.size(); i++) {
-            if (!stringContainsChar(guess, word[i])) cout << "_";
+            if (!stringContainsChar(guess, word[i])) {
+                guessed = false;
+                cout << "_";
+            }
             else cout << word[i];
         }
-        cout << "     ";
-        cin >> tmp;
+        if (guessed) {
+            cout << "\nYOU WON!";
+            return 0;
+        }
+        char tmp = _getch();
+        cout << "\n";
         guess += tmp;
 
     }
-    cout << "YOU WON";
 }
 bool stringContainsChar(std::string str, char c) {
     return str.find(c) != std::string::npos;
